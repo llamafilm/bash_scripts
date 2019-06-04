719c9f1fd978d1a401133c6b9a16068363c556b4
llamafilm <3991046+llamafilm@users.noreply.github.com>
2019-06-04 03:03:58 -0700

    Update startEC2.py

--- startEC2.py
+++ startEC2.py
@@ -18,7 +18,7 @@ def check_socket(host, port):
         else:
             return False
 
-def startInstance(i):
+def startInstance():
   # get Name tag
   for tag in i.tags:
     if tag['Key'] == 'Name':
@@ -28,7 +28,7 @@ def startInstance(i):
   state = i.state['Name']
   if state == 'running':
     print('Instance is already running')
-    return i.public_ip_address
+    return
   elif state in ['pending', 'shutting-down', 'stopping']:
     print('Instance is currently ' + state + '. Please try again later.')
     sys.exit()  
@@ -41,7 +41,7 @@ def startInstance(i):
       time.sleep(10)
       i.wait_until_running(WaiterConfig = {'Delay': 2})
       i.reload()
-      return i.public_ip_address
+      return
 
 print('Which instance would you like to start?')
 
@@ -54,24 +54,23 @@ for counter, object in enumerate(ec2.instances.all()):
       instanceName = tag['Value']
   print('(' + str(counter) + ')', object.instance_id + '\t' + object.instance_type + '\t' + 
     object.state['Name'] + '\t\t' + instanceName)
-# get user input
 print()
-choice = int(input())
-
-publicIP = startInstance(list[choice])
-print('Public IP: ' + publicIP)
+# get user input
+i = list[ int(input()) ]
+startInstance()
+print('Public IP: ' + i.public_ip_address)
 
 # login with SSH using correct username
 user = 'centos'
-for tag in list[choice].tags:
+for tag in i.tags:
   if tag['Key'] == 'SSH user':
     user = tag['Value']
     break
-command = 'ssh ' + user + '@' + publicIP
+command = 'ssh ' + user + '@' + i.public_ip_address
 print('Logging in after server is up... ', command)
 
 # wait for open port
-while not check_socket(publicIP,22):
+while not check_socket(i.public_ip_address,22):
   time.sleep(1)
 os.system(command)
 os.system('osascript -e "tell application \\"Terminal\\" to set current settings of window 1 to default settings"')
